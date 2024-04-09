a6f1e97e4b81f28612c96af430cdb4d238997bd0
diethermbp <diethercabelin@gmail.com>
2024-04-09 15:54:28 +0800

    added paged response

---
+++ ProductAPI/DTOs/PagedResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProductAPI.DTOs
+{
+    public record PagedResponseDto<T>
+    {
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public int TotalPages { get; init; }
+        public int TotalRecords { get; init; }
+        public List<T> Data { get; init; }
+    }
+}
+
